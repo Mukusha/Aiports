9ca842c58cce07a6a58b2535c356f1cc6e45a072
Mukusha <91588607+Mukusha@users.noreply.github.com>
2022-03-17 08:54:08 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
     private static int NCol = 2; // задаем столбец по которому будет проходить фильтрация
 
     public static void main(String[] args) {
-        if (args.length>0) NCol=Integer.parseInt(args[0]); //если столбец задан из командной строки
+        if (args.length>0) {  //если столбец задан из командной строки
+             if  (args[0].matches("\\d+"))  NCol=Integer.parseInt(args[0]);
+             else System.out.println("Некорректно введен номер столбца. Используется столбец по умолчанию: "+NCol);
+        }
         String filter = Input.inputStr(NCol);   // считать строку из консоли
         long start = System.currentTimeMillis();  //запуск таймера
         ArrayList<String> list = SearchByColumn.find(NCol,filter); //  фильтрация по столбцу
