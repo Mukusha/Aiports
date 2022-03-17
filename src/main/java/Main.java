b0e4cd4c41541c36ca5513531fe523689aac425f
Mukusha <91588607+Mukusha@users.noreply.github.com>
2022-03-17 08:32:44 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 public class Main {
     private static int NCol = 2; // задаем столбец по которому будет проходить фильтрация
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) {
         if (args.length>0) NCol=Integer.parseInt(args[0]); //если столбец задан из командной строки
         String filter = Input.inputStr(NCol);   // считать строку из консоли
         long start = System.currentTimeMillis();  //запуск таймера
@@ -15,7 +13,4 @@ public class Main {
         ArrayList<String>  listSort = Sorting.sorted(NCol,list); // сортировка по столбцу
         Print.print(listSort, timer); // вывод на экран
     }
-
-
-
 }
